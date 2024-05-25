0bc2463fe13d9142fa49913632d2215f6ae07288
Lucas <lucask4@gmail.com>
2024-05-25 10:39:46 -0700

    Add reward locations for Tchita Uplands

--- template_pack-main/scripts/logic/logic.lua
+++ template_pack-main/scripts/logic/logic.lua
@@ -71,3 +71,11 @@ end
 function defeat_vossler()
     return Tracker:ProviderCountForCode('dawn_shard') > 0 and sandseas()
 end
+
+function archades()
+    return aero('arc_aero') or (Tracker:ProviderCountForCode('soul_ward_key') > 0 and sochen_cave_palace())
+end
+
+function sochen_cave_palace()
+    return Tracker:ProviderCountForCode('soul_ward_key') > 0 and (tchita_uplands() or archades())
+end
