8712463282d42f1448d64d8e7f15b193e98e8222
Lucas <lucask4@gmail.com>
2024-05-26 14:47:15 -0700

    Add clan rank and hunt rewards to Clan Hall

--- template_pack-main/scripts/init.lua
+++ template_pack-main/scripts/init.lua
@@ -19,6 +19,7 @@ ScriptHost:LoadScript("scripts/utils.lua")
 ScriptHost:LoadScript("scripts/logic/logic.lua")
 ScriptHost:LoadScript("scripts/logic/aeropass.lua")
 ScriptHost:LoadScript("scripts/logic/huntclub.lua")
+ScriptHost:LoadScript("scripts/logic/hunts.lua")
 
 -- Custom Items
 ScriptHost:LoadScript("scripts/custom_items/class.lua")
@@ -28,6 +29,7 @@ ScriptHost:LoadScript("scripts/custom_items/progressiveTogglePlusWrapper.lua")
 -- Items
 Tracker:AddItems("items/items.json")
 Tracker:AddItems("items/trophy_rares.json")
+Tracker:AddItems("items/hunts.json")
 
 if not IS_ITEMS_ONLY then -- <--- use variant info to optimize loading
     -- Maps
