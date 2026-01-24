c6dd03d8e6d3e5f96405c32ccc9768e1d5da1deb
Katelyn Gigante <clockwork.singularity@gmail.com>
2026-01-24 20:57:34 +1100

    More lambda work

--- mapping_generator/build_locations.py
+++ mapping_generator/build_locations.py
@@ -1,3 +1,4 @@
+from collections import Counter
 import glob
 import os
 import sys
@@ -23,7 +24,10 @@ def main() -> None:
         pt_locations = json.load(loc_file)
 
     with open("./mapping_generator/lambda_to_access_rule.json", 'r') as f:
-        lambda_to_access_rule = json.load(f)
+        lambda_to_access_rule_full = json.load(f)
+
+    lambda_to_access_rule = lambda_to_access_rule_full.get("inactive", {}) | lambda_to_access_rule_full.get("needed", {}) |  lambda_to_access_rule_full.get("active", {})
+    lambda_counter = Counter()
 
     regions = {v['name']: v for v in pt_locations[0]['children']}
     all_locations = {}
@@ -84,6 +88,7 @@ def main() -> None:
         rule = rule_data_table.get(name)
         rulep = from_lambda.parse_lambda(rule)
         rule_str = from_lambda.to_str(rulep)
+        lambda_counter[rule_str] += 1
         access_rule = lambda_to_access_rule.setdefault(rule_str, None)
         if access_rule is not None and difficulty:
             access_rule += f',[$scaled_difficulty|{difficulty}]'
@@ -114,8 +119,21 @@ def main() -> None:
             all_names.append("Main/" + region['name'] + "/" + section['name'])
         pass
 
+    lambda_to_access_rule_full = {
+        "active": {},
+        "needed": {},
+        "inactive": {},
+    }
+    for rule_str, access_rule in lambda_to_access_rule.items():
+        count = lambda_counter[rule_str]
+        if count > 0 and access_rule is not None:
+            lambda_to_access_rule_full["active"][rule_str] = access_rule
+        elif count > 0:
+            lambda_to_access_rule_full["needed"][rule_str] = access_rule
+        elif access_rule is not None:
+            lambda_to_access_rule_full["inactive"][rule_str] = access_rule
     with open("./mapping_generator/lambda_to_access_rule.json", 'w') as f:
-        json.dump(lambda_to_access_rule, f, indent=4)
+        json.dump(lambda_to_access_rule_full, f, indent=4, sort_keys=True)
         f.write('\n')
     with open("./locations/locations.json", 'w') as loc_file:
         json.dump(pt_locations, loc_file, indent=2)
